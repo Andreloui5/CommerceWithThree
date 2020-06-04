60488c0fbbb1b4afeb7c1be7d21d55a04f6dc1bc
Craig Gant <craigkgant@gmail.com>
2020-06-04 17:27:13 -0400

    finished initial integrations with commercejs and three

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Commerce from "@chec/commerce.js";
+import ItemCard from "./components/ItemCard";
+
+const commerce = new Commerce(
+  "pk_test_18265006f98e5bc6f77efa3b7d99014bf7e1a31d0e6a3"
+);
 
 function App() {
+  // state is used to store the product's list so it can be rendered
+  const [products, setProducts] = useState([]);
+
+  /* useEffect will fetch the data after initial render, sets the state with the
+  fetched data, and then rerenders with the state*/
+  useEffect(() => {
+    commerce.products.list().then((res) => {
+      setProducts(res.data);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div>
+    //   <Container className="gallery">
+    <div>
+      {products.map((product) => (
+        <ItemCard key={product.id} {...product} />
+      ))}
     </div>
+    //   </Container>
+    // </div>
   );
 }
 
